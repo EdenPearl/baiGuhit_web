3ee129251f29cd946fc6e66ebbb813ca7e744db2
EdenPearl <126159727+EdenPearl@users.noreply.github.com>
2026-04-16 09:52:29 +0800

    updatde

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 
 const About = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <AboutContainer>
